86e081d20d9cc5b09a30998a7254ccb1d89ed3d4
Kimbumsoo99 <show7441@naver.com>
2024-03-27 14:30:21 +0900

    feat: MemberDao 추가

--- src/main/java/com/tripor/member/model/service/MemberService.java
+++ src/main/java/com/tripor/member/model/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.tripor.member.model.service;
 
-public interface MemberService {
+import com.tripor.member.model.dto.MemberDto;
 
+public interface MemberService {
+	int join(MemberDto memberDto);
+	MemberDto login(String userId, String userPw);
+	String findPassword(String userId);
 }
--- src/main/java/com/tripor/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/tripor/member/model/service/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.tripor.member.model.service;
 
 import com.tripor.member.model.dao.MemberDao;
 import com.tripor.member.model.dao.MemberDaoImpl;
+import com.tripor.member.model.dto.MemberDto;
 
 public class MemberServiceImpl implements MemberService{
 	private static MemberService instance = new MemberServiceImpl();
@@ -12,4 +13,22 @@ public class MemberServiceImpl implements MemberService{
 		return instance;
 	}
 
+	@Override
+	public int join(MemberDto memberDto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDto login(String userId, String userPw) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findPassword(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
